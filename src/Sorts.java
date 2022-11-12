74f05e4a6a2aec83b3507b8f980c5a4d74562a20
Nathan <ndbrewer@mtu.edu>
2022-11-11 21:16:46 -0500

    commit

--- src/Sorts.java
+++ src/Sorts.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sorts {
@@ -5,7 +6,20 @@ public class Sorts {
 
    public static <E extends Comparable<E>> void bubblesort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
       if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
-      //bannana
+      
+      for(int k = 0; k < highindex; k++){
+         for(int i = lowindex; i < highindex; i++){
+            if(list.get( i ).compareTo( list.get(i-1) ) > 0 && sortOrder == SortOrder.ASCENDING){
+               E data = list.get( i );
+               list.set( i,list.get( i-1 ) );
+               list.set( i-1,data );
+            } else if(list.get( i ).compareTo( list.get(i-1) ) < 0 && sortOrder == SortOrder.DESCENDING){
+               E data = list.get( i );
+               list.set( i,list.get( i-1 ) );
+               list.set( i-1,data );
+            }
+         }
+      }
    }
 
    public static <E extends Comparable<E>> void insertionsort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
---
+++ src/SortsTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortsTest {
+	
+	@Test
+	public void bubblesort() {
+	}
+	
+	@Test
+	public void insertionsort() {
+	}
+	
+	@Test
+	public void selectionsort() {
+	}
+	
+	@Test
+	public void mergesort() {
+	}
+	
+	@Test
+	public void mysort() {
+	}
+}

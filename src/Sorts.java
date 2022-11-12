0f08800be1a4a41421b8e9a8c1c3065f3a3ba1b1
Nathan <ndbrewer@mtu.edu>
2022-11-11 21:33:31 -0500

    commit

--- src/Sorts.java
+++ src/Sorts.java
@@ -8,7 +8,7 @@ public class Sorts {
       if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
       
       for(int k = 0; k < highindex; k++){
-         for(int i = lowindex; i < highindex; i++){
+         for(int i = lowindex+1; i < highindex; i++){
             if(list.get( i ).compareTo( list.get(i-1) ) > 0 && sortOrder == SortOrder.ASCENDING){
                E data = list.get( i );
                list.set( i,list.get( i-1 ) );
--- src/SortsTest.java
+++ src/SortsTest.java
@@ -1,11 +1,43 @@
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class SortsTest {
 	
 	@Test
-	public void bubblesort() {
+	public void bubblesort0() {
+		List< Integer > start = new ArrayList<>( Arrays.asList( 5,4,3,2,1 ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList( 5,4,3,2,1 ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList( 1,2,3,4,5 ) );
+		Sorts.bubblesort(list,0,5,Sorts.SortOrder.DESCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: bubblesort(%s) -> %s, expected: %s",start,list,expected ));
+ 	}
+	@Test
+	public void bubblesort1() {
+		List< Integer > start = new ArrayList<>( Arrays.asList( 1,2,3,4,5 ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList( 1,2,3,4,5 ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList( 5,4,3,2,1 ) );
+		Sorts.bubblesort(list,0,5,Sorts.SortOrder.ASCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: bubblesort(%s) -> %s, expected: %s",start,list,expected ));
+	}
+	@Test
+	public void bubblesortNullArg() {
+		List< Integer > start = null;
+		List< Integer > list = null;
+		try {
+			Sorts.bubblesort(list,0,1,Sorts.SortOrder.DESCENDING);
+			fail(String.format( "FAILED: bubblesort(%s) did not throw IllegalArgumentException",start));
+		}catch ( IllegalArgumentException e ){
+			//expected
+		}
+		catch ( Exception e ){
+			fail(String.format( "FAILED: bubblesort(%s) threw %s",start,e));
+		}
 	}
 	
 	@Test

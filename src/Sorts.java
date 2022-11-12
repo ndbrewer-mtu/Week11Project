64185c4a15f0329517d02f59068c65d0864fda38
Nathan <ndbrewer@mtu.edu>
2022-11-11 20:54:47 -0500

    banana

--- src/Sorts.java
+++ src/Sorts.java
@@ -5,7 +5,7 @@ public class Sorts {
 
    public static <E extends Comparable<E>> void bubblesort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
       if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
-      
+      //bannana
    }
 
    public static <E extends Comparable<E>> void insertionsort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {

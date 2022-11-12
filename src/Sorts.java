e3e297f3bf13315d547a59c0116cc3d0c118d0c7
Nathan <ndbrewer@mtu.edu>
2022-11-11 20:53:19 -0500

    commi

---
+++ src/Sorts.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Sorts {
+   public enum SortOrder { ASCENDING, DESCENDING }
+
+   public static <E extends Comparable<E>> void bubblesort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
+      if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
+      
+   }
+
+   public static <E extends Comparable<E>> void insertionsort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
+      if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
+   }
+
+   public static <E extends Comparable<E>> void selectionsort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
+      if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
+   }
+
+   public static <E extends Comparable> void mergesort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
+      if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
+   }
+
+   public static <E extends Comparable<E>> void mysort ( List< E > list, int lowindex, int highindex, SortOrder sortOrder ) {
+      if(list == null  || sortOrder == null || (highindex < lowindex)) throw new IllegalArgumentException();
+   }
+}

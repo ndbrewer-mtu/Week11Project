9f9e53ac597e8897e504c4f2ce672417fb16906b
Nathan <ndbrewer@mtu.edu>
2022-11-16 15:29:18 -0500

    mergesort 2-6 tests added.

--- src/SortsTest.java
+++ src/SortsTest.java
@@ -279,6 +279,51 @@ public class SortsTest {
 		if(!list.equals( expected ))
 			fail(String.format( "FAILED: mergesort(%s) -> %s, expected: %s",start,list,expected ));
 	}
+	@Test
+	public void mergesort2() { // test single element arrays
+		List< Integer > start = new ArrayList<>( Arrays.asList( 5 ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList( 5 ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList( 5 ) );
+		Sorts.mergesort(list,0,1,Sorts.SortOrder.DESCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: mergesort(%s) -> %s, expected: %s",start,list,expected ));
+	}
+	@Test
+	public void mergesort3() { // test empty arrays
+		List< Integer > start = new ArrayList<>( Arrays.asList(  ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList(  ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList(  ) );
+		Sorts.mergesort(list,0,0,Sorts.SortOrder.DESCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: mergesort(%s) -> %s, expected: %s",start,list,expected ));
+	}
+	@Test
+	public void mergesort4() { // test even array
+		List< Integer > start = new ArrayList<>( Arrays.asList( 5,4,3,2,1,0 ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList( 5,4,3,2,1,0 ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList( 0,1,2,3,4,5 ) );
+		Sorts.mergesort(list,0,6,Sorts.SortOrder.ASCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: mergesort(%s) -> %s, expected: %s",start,list,expected ));
+	}
+	@Test
+	public void mergesort5() { // test duplicate in array
+		List< Integer > start = new ArrayList<>( Arrays.asList( 5,4,3,2,2,1 ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList( 5,4,3,2,2,1 ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList( 1,2,2,3,4,5 ) );
+		Sorts.mergesort(list,0,6,Sorts.SortOrder.ASCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: mergesort(%s) -> %s, expected: %s",start,list,expected ));
+	}
+	@Test
+	public void mergesort6() { // test already sorted array
+		List< Integer > start = new ArrayList<>( Arrays.asList( 1,2,3,4,5 ) );
+		List< Integer > list = new ArrayList<>( Arrays.asList( 1,2,3,4,5 ) );
+		List<Integer> expected = new ArrayList<>( Arrays.asList( 1,2,3,4,5 ) );
+		Sorts.mergesort(list,0,5,Sorts.SortOrder.ASCENDING);
+		if(!list.equals( expected ))
+			fail(String.format( "FAILED: mergesort(%s) -> %s, expected: %s",start,list,expected ));
+	}
 	//Merge Sort =====================================================
 	
 	@Test

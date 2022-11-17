9bd2b3eb75326f442a959db9d04fc509b7aac146
Nathan <ndbrewer@mtu.edu>
2022-11-16 22:49:56 -0500

    commit

--- src/SortsProfiler.java
+++ src/SortsProfiler.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Random;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,12 @@ import java.util.stream.Collectors;
  */
 public class SortsProfiler {
 	
-	public static long profile( Object[] arg, Function< Object[], ? > f){
+	public static int numIterations = 10;
+	
+	public static long profile( Object[] arg, Consumer< Object[]> c){
 		
 		long start = System.currentTimeMillis();
-		f.apply( arg );
+		c.accept( arg );
 		long end = System.currentTimeMillis();
 		return end-start;
 	}
@@ -30,14 +33,37 @@ public class SortsProfiler {
 	 * @param args Contains a list of command-line arguments.
 	 */
 	public static void main( String[] args ) {
-		System.out.println("Elements       bubblesort     ");
+		String ele="Elements", merg="mergesort",sel="selectionsort",ins="insertionsort";
+		System.out.printf("%15s  %15s  %15s  %15s%n",ele,merg,sel,ins);
 		for(int i = 0; i <= 20; i++ ){
-			List<Integer> list = new Random().ints((int)Math.pow( 2,i ),-256,256).boxed().collect( Collectors.toList());
+			System.out.printf( "%15d",(int) Math.pow( 2,i ) );
+			long Merge = 0;
+			for(int j = 0 ; j < numIterations; j++){
+				List<Integer> list = new Random().ints((int)Math.pow( 2,i ),-256,256).boxed().collect( Collectors.toList());
+				Object[] arg = {list,0,list.size(), Sorts.SortOrder.ASCENDING};
+				Merge += profile( arg, Sorts::mergesort );
+			}
+			System.out.printf( "%15dms", Merge / numIterations);
 			
-			Object[] arg = {list,0,list.size(), Sorts.SortOrder.ASCENDING};
+			long Insertion = 0;
+			for(int j = 0 ; j < numIterations; j++){
+				List<Integer> list = new Random().ints((int)Math.pow( 2,i ),-256,256).boxed().collect( Collectors.toList());
+				Object[] arg = {list,0,list.size(), Sorts.SortOrder.ASCENDING};
+				Insertion += profile( arg, Sorts::insertionsort );
+			}
+			System.out.printf( "%15dms", Insertion / numIterations);
 			
-			System.out.printf( "%d",1 );
-			System.out.printf( "%10d", profile( arg, Sorts::mergesort ));
+			long Selection = 0;
+			for(int j = 0 ; j < numIterations; j++){
+				List<Integer> list = new Random().ints((int)Math.pow( 2,i ),-256,256).boxed().collect( Collectors.toList());
+				Object[] arg = {list,0,list.size(), Sorts.SortOrder.ASCENDING};
+				Selection += profile( arg, Sorts::selectionsort );
+			}
+			System.out.printf( "%15dms", Selection / numIterations);
+			
+			
+			
+			System.out.println();
 		}
 		
 		
